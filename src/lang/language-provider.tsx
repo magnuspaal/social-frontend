4401441369b14a399d2e7f58956de4cd2e6df3b0
magnuspaal <magnuspaal@gmail.com>
2024-04-23 14:49:46 +0300

    fix: get language from browser

--- src/lang/language-provider.tsx
+++ src/lang/language-provider.tsx
@@ -5,8 +5,11 @@ import { defaultLocale } from "."
 export const LanguageContext = createContext<{locale: any}>({locale: undefined})
 
 export function LanguageProvider({ children }: { children: React.ReactNode }) {
+
+  const lang = navigator.language.substring(0,2)
+
   return (
-    <LanguageContext.Provider value={{locale: defaultLocale}}>
+    <LanguageContext.Provider value={{locale: lang ?? defaultLocale}}>
       {children}
     </LanguageContext.Provider>
   )
